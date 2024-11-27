5cf7f9eb8e58aefaccf34c2f591292f54571129e
juliettehnr <juliette.hnr.pro@gmail.com>
2024-11-27 11:36:08 +0100

    ajout script anglais

---
+++ programmes/script_ang.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]
+then
+  echo "Le script prend exactement 1 argument"
+  exit 1
+fi
+
+fichier_url=$1
+num_ligne=1
+
+echo "<html>
+<head>
+<meta charset='UTF-8'>
+</head>
+<body>
+<table>
+<thead>
+<tr>
+<th>N° de ligne</th>
+<th>URL</th>
+<th>Type d'encodage</th>
+<th>Nombre de mots</th>
+</tr>
+</thead>
+<tbody>" > tableau_ang.html
+
+while read -r line
+do
+	http_code=$(curl -s -I -L -w "%{http_code}" -0 /dev/null $line)
+ 	encoding=$(curl -s -I -L -w "%{content_type}" -0 /dev/null $line | grep -P -o "charset=\S+" | cut -d'=' -f2) 
+  
+ 	if [ -z "$encoding" ]
+ 	then
+	encoding="N/A"
+	fi 
+
+	nbmots=$(lynx "$line" -dump -nolist | wc -w)
+	echo -e "<tr><td>$num_ligne</td><td>$line</td><td>$encoding</td><td>$nbmots</td></tr>" >> tableau_ang.html
+	num_ligne=$(expr $num_ligne + 1)
+
+done < $fichier_url
+
+echo "</tbody>
+</table>
+</body>
+</html>" >> tableau_ang.html
